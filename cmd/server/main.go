7e970a5a87fe2ee554b239361f903ef9e8da3740
Justin Largo <justinlargo7@gmail.com>
2026-01-15 16:46:05 -0500

    added stomp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	conn := "amqp://guest:guest@localhost:5672/"
+	rabbit, err := amqp.Dial(conn)
+	if err != nil {
+		log.Fatal("oops", err)
+	}
+	defer rabbit.Close()
+	fmt.Println("Successfully started the server!")
+
+	// Watch for interrupt
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
