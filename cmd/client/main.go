25fd38baf22948a94fba92d16fcd0a6f52ff4cc2
Justin Largo <justinlargo7@gmail.com>
2026-01-18 11:31:02 -0500

    added transient pause queues to client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	// Connect
+	conn := "amqp://guest:guest@localhost:5672/"
+	rabbit, err := amqp.Dial(conn)
+	if err != nil {
+		log.Fatal("not good!", err)
+	}
+	defer rabbit.Close()
+	fmt.Println("Connected to the server!")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatal("bad username", err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		rabbit,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+username,
+		routing.PauseKey,
+		pubsub.TransientQueueType,
+	)
+	if err != nil {
+		log.Fatal("no chan or queue womp womp", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
--- internal/pubsub/pub.go
+++ internal/pubsub/pub.go
@@ -33,3 +33,48 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 
 	return err
 }
+
+type SimpleQueueType string
+
+const (
+	DurableQueueType   SimpleQueueType = "durable"
+	TransientQueueType SimpleQueueType = "transient"
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	shouldAutoDelete := false
+	isDurable := false
+	isExclusive := false
+
+	if queueType == DurableQueueType {
+		isDurable = true
+	}
+	if queueType == TransientQueueType {
+		shouldAutoDelete = true
+		isExclusive = true
+	}
+
+	queue, err := channel.QueueDeclare(
+		queueName,
+		isDurable,
+		shouldAutoDelete,
+		isExclusive,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	return channel, queue, nil
+}
